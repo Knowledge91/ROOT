826e84dd365f374aeba426f9b3924407a4fec7af
Knowledge <dirkhornung91@gmail.com>
2016-11-23 12:17:49 +0100

    Initial Commit

---
+++ Root/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Root
+//
+//  Created by Dirk Hornung on 23/11/16.
+//  Copyright © 2016 Dirk Hornung. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
